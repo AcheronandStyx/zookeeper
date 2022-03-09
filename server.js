32f51e607f88c0e23175bc5d32e4683d5c7d4e58
AcheronandStyx <matthew.danielson@trojans.dsu.edu>
2022-03-08 22:34:30 -0600

    11.1 done

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
 const express = require('express');
-const { appendFile } = require('fs');
+
 const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
-
 // We assign express() to the app variable so that we can later chain on methods to the Express.js server.
 const app = express();
 
 // This function will take in req.query as an argument and filter through the animals accordingly, returning the new filtered array
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
-     // Note that we save the animalsArray as filteredResults here:
+    // Note that we save the animalsArray as filteredResults here:
     let filteredResults = animalsArray;
     if (query.personalityTraits) {
         // Save personalityTraits as a dedicated array.
         // If personalityTraits is a string, place it into a new array and save.
         if (typeof query.personalityTraits === 'string') {
-          personalityTraitsArray = [query.personalityTraits];
+            personalityTraitsArray = [query.personalityTraits];
         } else {
-          personalityTraitsArray = query.personalityTraits;
+            personalityTraitsArray = query.personalityTraits;
         }
         // Loop through each trait in the personalityTraits array:
         personalityTraitsArray.forEach(trait => {
-          // Check the trait against each animal in the filteredResults array.
-          // Remember, it is initially a copy of the animalsArray,
-          // but here we're updating it for each trait in the .forEach() loop.
-          // For each trait being targeted by the filter, the filteredResults
-          // array will then contain only the entries that contain the trait,
-          // so at the end we'll have an array of animals that have every one 
-          // of the traits when the .forEach() loop is finished.
-          filteredResults = filteredResults.filter(
-            animal => animal.personalityTraits.indexOf(trait) !== -1
-          );
+            // Check the trait against each animal in the filteredResults array.
+            // Remember, it is initially a copy of the animalsArray,
+            // but here we're updating it for each trait in the .forEach() loop.
+            // For each trait being targeted by the filter, the filteredResults
+            // array will then contain only the entries that contain the trait,
+            // so at the end we'll have an array of animals that have every one 
+            // of the traits when the .forEach() loop is finished.
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
         });
-      }
+    }
     if (query.diet) {
         filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
     }
@@ -44,20 +43,34 @@ function filterByQuery(query, animalsArray) {
         filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
     return filteredResults;
-}
+};
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 /*
 get() method requires two arguments. The first is a string that describes the route the client will have to fetch from. 
 The second is a callback function that will execute every time that route is accessed with a GET request.
 
 The second takeaway is that we are using the send() method from the res parameter (short for response) to send the string Hello! to our client.
 */
-app.get('/api/animals', (req, res) => {
+app.get('/api/animals', (req, res) => { // get the entire file
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
 });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
 //queryu parameters in the url will become JSON on the return
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
